62bf032241150087e37c1a7519aac23208f14f16
benj <bmp22@protonmail.com>
2021-04-19 20:18:01 +0200

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ViewComponent} from './word/view/view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/',
+    component: ViewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
